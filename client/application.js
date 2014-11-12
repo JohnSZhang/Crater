6cd2d0a2d00077e76f4818f3c22bfc49831cc041
John Zhang <john.shimin.zhang@gmail.com>
2014-11-12 15:33:04 -0500

    add fixtures and first tempate

--- client/application.js
+++ client/application.js
@@ -3,8 +3,6 @@ if (Meteor.isClient) {
   $(function(){
     console.log(Posts.find().count())
   })
-  console.log('loading template')
-  console.log(Posts)
   Template.body.helpers({
     posts: function () {
       return Posts.find();
