36f5e10a20a4510edc4b7440e324b72405d821e6
App Academy Student <appacademy@Apps-Mac-mini-31.local>
2014-11-12 18:58:39 -0500

    made everything pretty

--- client/application.js
+++ client/application.js
@@ -2,15 +2,23 @@ if (Meteor.isClient) {
   // This code only runs on the client
   $(function(){
     console.log(Posts.find().count())
-  })
+  });
+
   Template.posts.helpers({
     posts: function () {
       return Posts.find();
     }
   });
+
   Template.body.helpers({
     userEmail: function () {
       return Meteor.user().emails[0].address
+    }
+  });
+
+  Template.body.events({
+    "click button.new-post": function (event) {
+      Router.go("/posts/new")
     }
   })
 }
