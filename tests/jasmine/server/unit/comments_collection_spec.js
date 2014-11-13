7c3a7ff2ee79cf6e5f47265c93bb7321faeab73b
John Zhang <john.shimin.zhang@gmail.com>
2014-11-13 03:50:04 -0500

    comments, haf broken still

--- tests/jasmine/server/unit/comments_collection_spec.js
+++ tests/jasmine/server/unit/comments_collection_spec.js
@@ -42,11 +42,26 @@ describe("Post Comment Methods", function(){
 
   describe("deleteComment", function(){
     it ("Should be deleteable", function(){
+      var docId = "abc";
+      var data = { data: "old comment" };
+
+      spyOn(Meteor, "userId").and.returnValue("user id");
+      spyOn(Posts, "find").and.returnValue("user id");
+
+      CommentsService.deleteComment(docId, data);
+
+      expect(Posts.update).toHaveBeenCalledWith("abc", { $pull: { body:
+      "old comment" }}, jasmine.any(Function));
 
     });
 
-    it ("Should only be deletable by current user", function(){
+    it ("Should only be deletable by comment Owner", function(){
+      var docId = "abc";
+      spyOn(Meteor, "userId").and.returnValue("2");
 
+      expect(function(){
+        CommentsService.deleteComment(docId)
+      }).toThrow();
     });
   })
 })
